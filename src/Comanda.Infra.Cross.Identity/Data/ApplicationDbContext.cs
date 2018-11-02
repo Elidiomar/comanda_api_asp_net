b1b99d52031078d4f470dc8cdae96e59d00055c0
Elidiomar Rodrigues <elidiomar.rodrigues@gmail.com>
2018-11-02 15:24:30 -0300

    Update ApplicationDbContext.cs

--- src/Comanda.Infra.Cross.Identity/Data/ApplicationDbContext.cs
+++ src/Comanda.Infra.Cross.Identity/Data/ApplicationDbContext.cs
@@ -43,53 +43,5 @@ namespace Comanda.Infra.Cross.Identity.Data
             // define the database to use
             optionsBuilder.UseSqlServer(config.GetSection("ConnectionString")["LocalConnection"]);
         }
-
-        //public void Initialize()
-        //{
-        //    if (_context.Database.EnsureCreated())
-        //    {
-        //        if (!_roleManager.RoleExistsAsync(Roles.ROLE_API_ALTURAS).Result)
-        //        {
-        //            var resultado = _roleManager.CreateAsync(
-        //                new IdentityRole(Roles.ROLE_API_ALTURAS)).Result;
-        //            if (!resultado.Succeeded)
-        //            {
-        //                throw new Exception(
-        //                    $"Erro durante a criação da role {Roles.ROLE_API_ALTURAS}.");
-        //            }
-        //        }
-
-        //        CreateUser(
-        //            new ApplicationUser()
-        //            {
-        //                UserName = "admin_apialturas",
-        //                Email = "admin-apialturas@teste.com.br",
-        //                EmailConfirmed = true
-        //            }, "AdminAPIAlturas01!", Roles.ROLE_API_ALTURAS);
-
-        //        CreateUser(
-        //            new ApplicationUser()
-        //            {
-        //                UserName = "usrinvalido_apialturas",
-        //                Email = "usrinvalido-apialturas@teste.com.br",
-        //                EmailConfirmed = true
-        //            }, "UsrInvAPIAlturas01!");
-        //    }
-        //}
-
-        //private void CreateUser(ApplicationUser user,string password,string initialRole = null)
-        //{
-        //    if (_userManager.FindByNameAsync(user.UserName).Result == null)
-        //    {
-        //        var resultado = _userManager
-        //            .CreateAsync(user, password).Result;
-
-        //        if (resultado.Succeeded &&
-        //            !String.IsNullOrWhiteSpace(initialRole))
-        //        {
-        //            _userManager.AddToRoleAsync(user, initialRole).Wait();
-        //        }
-        //    }
-        //}
     }
 }
